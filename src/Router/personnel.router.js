20a2c27a076a71cbf967488620d4057dc4970878
huunong <nonghuunam2002@gmail.com>
2024-07-05 22:23:53 +0700

    update notification

--- src/Router/personnel.router.js
+++ src/Router/personnel.router.js
@@ -1,11 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const Personnel_Controller = require("../Controller/personnel.controller")
-
-router.post("/register",Personnel_Controller.create_Personnel)
 router.get("/get-all-personnel",Personnel_Controller.get_All_Personnel)
 router.get("/get-all-personnel/:search",Personnel_Controller.get_Personnel_With_Filter)
-router.post("/login-personnel",Personnel_Controller.login_Personnel)
 router.put("/update-personnel/:personnel_id",Personnel_Controller.update_Personnel)
 router.delete("/delete-personnel/:personnel_id",Personnel_Controller.delete_Personnel)
 module.exports = router
