75441687eae9dfb7654d581520ea834763e55e56
huunong <nonghuunam2002@gmail.com>
2024-07-11 08:45:51 +0700

    update send notification

--- src/Common/firebaseadminconfig.js
+++ src/Common/firebaseadminconfig.js
@@ -1,8 +1,8 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json'); // Đường dẫn đến tệp JSON đã tải xuống
+// const admin = require('firebase-admin');
+// const serviceAccount = require('./serviceAccountKey.json'); // Đường dẫn đến tệp JSON đã tải xuống
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
 
-module.exports = admin;
+// module.exports = admin;
